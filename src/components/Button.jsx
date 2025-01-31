884e501b01e995ad5fd9ba6ae135d05b28b3b6a3
Julia Reva <98896958+juliareva@users.noreply.github.com>
2025-01-30 16:50:43 -0800

    Sign Up section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,14 +4,17 @@ const Button = ({
   backgroundColor,
   borderColor,
   textColor,
+  fullWidth
 }) => {
   return (
     <button
-    className={`rounded-full flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-      ${backgroundColor 
-      ? `${backgroundColor} ${textColor} ${borderColor}` 
-      : 'bg-coral-red text-white border-coral-red'}`}
-  >
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
     {label}
     {iconURL && (
       <img
